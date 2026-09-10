fe443c1541b8ef3380f5f6629c3cb0453c28ae74
Aluno <kelvinmaiaml@gmail.com>
2026-09-10 20:36:58 -0300

    first commit

---
+++ aula1009.js
@@ -0,0 +1,10 @@
+var dinheiro = true
+var comida = "acarajé"
+
+if(dinheiro== true && comida == "acarajé"){
+    console.log("Eba, chegou seu pedido")
+} else if(dinheiro==false || comida != "acarajé"){
+    console.log("Era uma vez, fique com fome")
+}else{
+    console.log("não vai comer nada, só lamento!")
+}
